76a89cf5960fc5a9e413eb7c441eccca3f4a7526
maziar <sojodyan@gmail.com>
2025-01-14 10:46:16 -0500

    import random

--- Week02/labs/Tue/lab2.py
+++ Week02/labs/Tue/lab2.py
@@ -1,3 +1,5 @@
+import random
+
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "carbon"]
 print("Elements: ", elements)
 #  git add . && git commit -m "add elements array" && git push
