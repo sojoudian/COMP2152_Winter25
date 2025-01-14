17add410759e23e7b3c6f166822ec9bbf35f7988
maziar <sojodyan@gmail.com>
2025-01-14 10:27:35 -0500

    add a few example with Py func

--- Week02/labs/Tue/lab2.py
+++ Week02/labs/Tue/lab2.py
@@ -4,4 +4,7 @@ print("Elements: ", elements)
 
 # def funct_name():
 #     return True
-
+# def say_greeting(name, message="hi"):
+#     print(f" {message}, {name}")
+# say_greeting("Maziar")
+# say_greeting("Maziar", "Hello")
