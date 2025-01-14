4cd689e7db2e8c1642ee23111d144710598db75e
maziar <sojodyan@gmail.com>
2025-01-14 11:04:21 -0500

    fix int typo

--- Week02/labs/Tue/lab2.py
+++ Week02/labs/Tue/lab2.py
@@ -1,6 +1,6 @@
 import random
 
-elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "carbon"]
+elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("Elements: ", elements)
 #  git add . && git commit -m "add elements array" && git push
 
