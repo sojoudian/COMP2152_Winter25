db5ebcc52f6b815af6672b407721a005051ec53d
maziar <sojodyan@gmail.com>
2025-01-17 11:06:30 -0500

    add lab2 week 2 10 to 12

--- Week02/labs/Friday/10am12pm/lab02.py
+++ Week02/labs/Friday/10am12pm/lab02.py
@@ -2,5 +2,8 @@ import random
 def number_gussing_game():
     targetNumber = random.randint(1, 100)
 
+    play = input("Do you want to play number gussing gmae? (yes/no)").strip().lower()
+    
+
     play = input()
     return True
