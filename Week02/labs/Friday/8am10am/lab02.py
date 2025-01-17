b520a48c7144e2c5c378da47d7d3153265c0c5a3
maziar <sojodyan@gmail.com>
2025-01-17 08:33:59 -0500

    add arr and main func

---
+++ Week02/labs/Friday/8am10am/lab02.py
@@ -0,0 +1,11 @@
+#define the choices array
+choices = ["Rock", "Paper", "Scissors"]
+
+def main():
+    user_input = input("Enter your choice (Rock, Paper, Scissors)").capitalize()
+
+    return True
+
+# Run the game
+if __name__ == "main":
+    main()
