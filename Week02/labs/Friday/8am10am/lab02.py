37a6419fe6ed07c81f87c63467613ecb105e4d2c
maziar <sojodyan@gmail.com>
2025-01-17 08:46:23 -0500

    print players choices and logic of tie

--- Week02/labs/Friday/8am10am/lab02.py
+++ Week02/labs/Friday/8am10am/lab02.py
@@ -13,8 +13,17 @@ def main():
     # Convert the user input to an index
     player_choice = choices.index(user_input)
 
-    
-    return True
+    # Randomly select the computer choice
+    computer_choice = random.randint(0,2)
+
+    print(f"Player chose {choices[player_choice]}")    
+    print(f"Computer chose {choices[computer_choice]}")    
+
+    # Determine the winer
+    if player_choice == computer_choice:
+        print("It's a tie!")
+            
+
 
 # Run the game
 if __name__ == "main":
