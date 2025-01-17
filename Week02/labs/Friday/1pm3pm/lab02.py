e189f50b0a297762c1283313cd8920c9da90cedf
maziar <sojodyan@gmail.com>
2025-01-17 14:09:44 -0500

    generate the randome choice from the elements list

--- Week02/labs/Friday/1pm3pm/lab02.py
+++ Week02/labs/Friday/1pm3pm/lab02.py
@@ -1,2 +1,15 @@
+import random
+
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon"]
 print(elements)
+
+
+randomElement = round.choice(elements)
+
+attemts = 3
+
+for attemt in range(attemts):
+    guess = input(f"Attempt {attemt + 1}/{attemts}: Guess the element: 
+                  ").strip()
+    if guess.lower() == randomElement.lower():
+        print(f"Correct! the element was {randomElement}.")
