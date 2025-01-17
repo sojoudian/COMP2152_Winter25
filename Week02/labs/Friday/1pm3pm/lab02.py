d324b228cc46023b2cc7a096aabddc24d2fdbc3c
maziar <sojodyan@gmail.com>
2025-01-17 14:14:14 -0500

    generate the randome choice from the elements list

--- Week02/labs/Friday/1pm3pm/lab02.py
+++ Week02/labs/Friday/1pm3pm/lab02.py
@@ -6,10 +6,14 @@ print(elements)
 
 randomElement = round.choice(elements)
 
-attemts = 3
+attempts = 3
 
-for attemt in range(attemts):
-    guess = input(f"Attempt {attemt + 1}/{attemts}: Guess the element: 
+for attempt in range(attempts):
+    guess = input(f"Attempt {attempt + 1}/{attempts}: Guess the element: 
                   ").strip()
     if guess.lower() == randomElement.lower():
         print(f"Correct! the element was {randomElement}.")
+    else:
+        if attempt < attempts -1:
+            print(f"Wrong guess! Hint: The element starts with
+                  '{randomElement[0]}', Try another time.")
