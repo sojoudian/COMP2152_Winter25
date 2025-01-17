d1c2d2dd10a901325d01b52ca2605937a4c3bf8e
maziar <sojodyan@gmail.com>
2025-01-17 14:22:44 -0500

    fix typos

--- Week02/labs/Friday/1pm3pm/lab02.py
+++ Week02/labs/Friday/1pm3pm/lab02.py
@@ -9,11 +9,12 @@ randomElement = round.choice(elements)
 attempts = 3
 
 for attempt in range(attempts):
-    guess = input(f"Attempt {attempt + 1}/{attempts}: Guess the element: 
-                  ").strip()
+    guess = input(f"Attempt {attempt + 1}/{attempts}: Guess the element: ").strip()
     if guess.lower() == randomElement.lower():
         print(f"Correct! the element was {randomElement}.")
     else:
-        if attempt < attempts -1:
-            print(f"Wrong guess! Hint: The element starts with
-                  '{randomElement[0]}', Try another time.")
+        if attempt < attempts - 1:
+                        print(f"Wrong! Hint: The element starts with '{randomElement[0]}'.
+                              Try again!")
+        else:
+            print(f"Wrong guess! The element was {randomElement}")    
