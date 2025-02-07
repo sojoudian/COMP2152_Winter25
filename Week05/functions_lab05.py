fb14f65b07065d516a0c519733f458665936f66d
maziar <sojodyan@gmail.com>
2025-02-06 23:43:02 -0500

    add week05 starter code

---
+++ Week05/functions_lab05.py
@@ -0,0 +1,30 @@
+# Question 3: Define the collect_loot() function.
+def collect_loot(belt, loot_options):
+    print("Collecting loot...")
+    if condition:
+        # Simulate loot collection by taking the first available item.
+
+
+
+    else:
+        print("No loot available!")
+    return 
+
+# Question 4: Define the use_loot() function.
+def use_loot():
+    print("    |    !!You see a monster in the distance! So you quickly use your first item:")
+    if condition:
+        
+        good_loot_options = ["Health Potion", "Leather Boots"]
+        bad_loot_options = ["Poison Potion"]
+        if condition:
+            
+            print("    |    You used " + first_item + " to increase your health to " + str(health_points))
+        elif conditions:
+            
+            print("    |    You used " + first_item + " which decreased your health to " + str(health_points))
+        else:
+            print("    |    You used " + first_item + " but nothing happened")
+    else:
+        print("No loot available to use!")
+    return 
