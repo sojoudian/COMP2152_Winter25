195990ac09dabbc11a66c341c91ad7f287b69956
maziar <sojodyan@gmail.com>
2025-03-04 10:34:39 -0500

    Set timeout of socket in seconds

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -1,4 +1,5 @@
 import platform
+import socket
 
 print("Current machine type")
 print(platform.machine()) # Apple M1, M2, M3, M4, Raspberry Pi : arm64; for intel/AMD: x86
@@ -6,4 +7,8 @@ print ("-------------------------------\n")
 
 print("Current Processor Type:")
 print(platform.architecture())
+print ("-------------------------------\n")
+
+print("Set timeout of socket in seconds")
+print(socket.setdefaulttimeout(50))
 print ("-------------------------------\n")
