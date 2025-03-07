ab81c2b8dbb15e9bda1a5e32eb82ec175010cca0
maziar <sojodyan@gmail.com>
2025-03-07 08:41:34 -0500

    add machine and processor type, set socket default timeout to 50

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -1,1 +1,14 @@
- 
+import platform
+import socket
+
+print("Current Machine Type")
+print(platform.machine()) 
+print("===============================")
+
+print("Current Processor Type")
+print(platform.architecture())
+print("===============================")
+
+print("")
+print(socket.setdefaulttimeout(50))
+print("===============================")
