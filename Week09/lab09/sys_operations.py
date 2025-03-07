b8babb34e313a762acdfdbfd9d36cfe61791de75
maziar <sojodyan@gmail.com>
2025-03-07 15:26:54 -0500

    add sys operations

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -1,8 +1,13 @@
 import platform
 import socket
+import os
+import sys
 
 print(f"\nMachine Type: {platform.machine()}")
 print(f"\nProcessor Type: {platform.architecture()}")
 
 socket.setdefaulttimeout(50)
 print(f"\nDefault Timeout for Socket: {socket.getdefaulttimeout()}")
+
+print(f"\nOS Type: {os.name()}")
+print(f"\nOS Name: {platform.system()}")
