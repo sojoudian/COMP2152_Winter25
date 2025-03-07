ad56f0da2dc90e4fd4c36bdadf64f89b120b046c
maziar <sojodyan@gmail.com>
2025-03-07 08:57:33 -0500

    add  file practice, file_handle to repo

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -1,5 +1,6 @@
 import platform
 import socket
+import os
 
 print("Current Machine Type")
 print(platform.machine()) 
@@ -15,3 +16,17 @@ print("Get the current Socket Timeout")
 print(socket.getdefaulttimeout())
 
 print("===============================")
+print("Get the current Operating System Type")
+print(os.name)
+print("Get the current Operating System Name")
+print(platform.system())
+
+print("===============================")
+print("Current Process ID")
+print(os.getpid())
+
+file_name = "fdpractice.txt"
+print(f"\n[Before Fork] Process {os.getpid()}")
+
+file_handle = os.open(file_name, os.O_RDWR | os.O_CREAT)
+print(f"\n[Process {os.getpid()}] Opened file_handle: {file_handle}")
