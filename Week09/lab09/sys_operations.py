ebf3ee80b46d6c345e4edebcb7f94b89e7730674
maziar <sojodyan@gmail.com>
2025-03-07 13:37:25 -0500

    add machine type, arch, os name, os type, and change default Socket time out

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -1,1 +1,12 @@
- 
+import platform
+import socket
+import os
+
+print(f"Machine Type: {platform.machine()}\n")
+print(f"CPU Architecture: {platform.architecture()}\n")
+
+socket.setdefaulttimeout(50)
+print(f"CPU Architecture: {socket.getdefaulttimeout()}\n")
+
+print(f"OS Type: {os.name}\n")
+print(f"OS name: {platform.system()}\n")
