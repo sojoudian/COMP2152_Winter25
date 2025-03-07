4af3a4d0ae689c1bc780b2cdb4fabf212a7de7ac
maziar <sojodyan@gmail.com>
2025-03-07 15:42:25 -0500

    add file handle using OS

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -12,3 +12,26 @@ print(f"\nDefault Timeout for Socket: {socket.getdefaulttimeout()}")
 print(f"\nOS Type: {os.name}")
 print(f"\nOS Name: {platform.system()}")
 print(f"\nCurrent PID: {os.getpid()}")
+
+file_name = "fdpractice.txt"
+file_handle = os.open(file_name, os.O_RDWR | os.O_CREAT)
+file_object_TextIO = os.fdopen(file_handle, "w+")
+file_object_TextIO.write("Some string to write to this file")
+file_object_TextIO.flush()
+
+# windows
+# pid = 1
+pid = os.fork()
+if pid == 0:
+    print(f"\n[Child PID: {os.getpid()}], [Parent PID: {os.getppid()}]")
+    os.lseek(file_handle, 0, 0)
+    print(f"\n[Child PID: {os.getpid()}] File Content: {os.read(file_handle, 100).decode()}")
+    os.close(file_handle)
+    sys.exit(0)
+else:
+    print(f"\n[Parent PID: {os.getpid()}], [Child PID: {pid}]")    
+    print("Wait for child")
+    os.wait()
+    print("child finished")
+    file_object_TextIO.close()
+sys.exit(0)    
