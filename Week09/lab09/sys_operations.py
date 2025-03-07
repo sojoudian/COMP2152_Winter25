78192c068085c31320ad53a119129b0b4636f48f
maziar <sojodyan@gmail.com>
2025-03-07 09:24:09 -0500

    add system operations

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -1,6 +1,7 @@
 import platform
 import socket
 import os
+import sys
 
 print("Current Machine Type")
 print(platform.machine()) 
@@ -35,3 +36,26 @@ file_object_TextIO = os.fdopen(file_handle, "w+")
 
 file_object_TextIO.write("Some string to write to the file")
 file_object_TextIO.flush()
+
+print(f"\n[Before  Process {os.getpid()}] Forking now")
+pid = os.fork()
+
+if pid == 0:
+    # Child Process
+    print(f"\n[Child PID {pid} has Parent Process ID: {os.getppid()}]")
+    os.lseek(file_handle, 0, 0)
+
+    print(f"[Child Process {os.getpid()}] File Content: {os.read(file_handle, 100).decode()}")
+
+    os.close(file_handle)
+    sys.exit(0)
+else:
+    # Parent PID
+    print(f"\n[Parent Process ID: {os.getpid}], Child PID: {pid}")
+    print("Wait for the Child to complete the modification")
+    os.wait()
+    print("Child Process Finished the modufication")
+    file_object_TextIO.close()
+
+print(f"\n[Process {os.getpid}] File Closed. Exiting now..")    
+sys.exit(0)
