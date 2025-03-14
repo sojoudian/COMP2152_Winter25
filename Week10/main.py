68a25554a44d08b0ff05b1cb1d77868de3ed64d4
maziar <sojodyan@gmail.com>
2025-03-14 10:50:10 -0400

    add week10 database, and main.py

--- Week10/main.py
+++ Week10/main.py
@@ -11,20 +11,21 @@ try:
                 query_1 = "SELECT * FROM demo WHERE id > 14"
                 cursor.execute(query_1)
                 rows = cursor.fetchall()
-                print("Name of rows with id > 14:\n")
+                print("Name of rows with id > 14")
                 for row in rows:
                     print(row["name"])
-            except Exception as e:
-                print(f"Error executing query_1: {e}")
-            # Delete Rows based on the user input                
+            except Exception as e:   
+                print(f"Error executing query 1: {e}")
+            # Delete one or more rows based on the user input    
             try:
-                del_row = int(input("Enter the row ID threshold for deletion:"))
+                del_row = int(input("Enter the row ID theshold for deletion: "))
                 query_2 = "DELETE FROM demo WHERE id < ?"
-                cursor.execute(query_2, (del_row,))
+                cursor.execute(query_2, (del_row, ))
                 num_rows = cursor.rowcount
                 print(f"{num_rows} rows affected.")
                 db_conn.commit()
-            except Exception as e:
-                print(f"Error executing query_1: {e}")                   
-except sqlite3.Error as e:    
-    print(f"Database connection error: {e}")
+            except Exception as e:   
+                print(f"Error executing query 1: {e}")    
+
+except sqlite3.Error as e:
+    print(f"Database Connection Error: {e}")
