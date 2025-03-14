2b4dc4345c5c9940b6479f429e8324aa83dfd85e
maziar <sojodyan@gmail.com>
2025-03-14 13:41:32 -0400

    add q1 and q2

--- Week10/main.py
+++ Week10/main.py
@@ -13,6 +13,16 @@ try:
                 for row in rows:
                     print(row["name"])
             except Exception as e:
-                print(f"Error Executing Query_1")                    
+                print(f"Error Executing Query_1")
+            # Delete Row based on User Input      
+            try:
+                del_row = int(input("Enter the row ID threshold for deletion: "))
+                query_2 = "DELETE FROM demo WHERE id < ?"
+                cursor.execute(query_2, (del_row, ))
+                num_rows = cursor.rowcount
+                print(f"{num_rows} row affected!")
+                db_conn.commit()
+            except Exception as e:
+                print(f"Error Executing Query_2")                           
 except sqlite3.Error as e:
     print(f"Database Connection Error: {e}")   
