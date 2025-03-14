a60c62a4e4fd13d00564a2cac321d3a894baee1a
maziar <sojodyan@gmail.com>
2025-03-14 08:54:49 -0400

    fix query 2 issue

--- Week10/main.py
+++ Week10/main.py
@@ -8,7 +8,7 @@ try:
         db_conn.row_factory = sqlite3.Row
         with closing(db_conn.cursor()) as cursor:
             try:
-                query_1 = "SELECT * from demo WHERE id > 14"
+                query_1 = "SELECT * FROM demo WHERE id > 14"
                 cursor.execute(query_1)
                 rows = cursor.fetchall()
                 print("Name of rows with id > 14:\n")
@@ -20,7 +20,7 @@ try:
             try:
                 del_row = int(input("Enter the row ID threshold for deletion:"))
                 query_2 = "DELETE FROM demo WHERE id < ?"
-                cursor.execute(query_2, (del_row))
+                cursor.execute(query_2, (del_row,))
                 num_rows = cursor.rowcount
                 print(f"{num_rows} rows affected.")
                 db_conn.commit()
