4b1dcdc3126897bc11909faf1cfff0c51454c41a
maziar <sojodyan@gmail.com>
2025-03-21 15:17:31 -0400

    add Person class

---
+++ Week11/Person.py
@@ -0,0 +1,19 @@
+class Person():
+    def __init__(self, p_name, p_age, p_height):
+        self.__name = p_name
+        self.__age = p_age
+        self.__height = p_height
+        self.public_prop = "I'm public"
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    def __del__(self):
+        print("The garbage collector is automatically destroying the person object")        
+
+person1 = Person("Mike", 20, 6)
+print("the name of the person is: " + str(person1.name))
